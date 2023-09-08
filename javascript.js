4bc20c2e6d240f9e5b1ff3fa150bf75fbc917b48
melinaDaendliker <melina.daendliker@hotmailc.om>
2023-09-08 20:55:10 +0200

    make the display working for simple calculations

--- javascript.js
+++ javascript.js
@@ -1,27 +1,25 @@
 const equal = document.querySelector('#equal');
 const numbers = document.querySelectorAll('.num');
 const operators = document.querySelectorAll('.op');
-
+const calculationDisplay = document.querySelector('.calculation');
+const result = document.querySelector('.solution');
 // default values
-let operant1 = [];
-let operant2 = [];
+let operant1 = "";
+let operant2 = "";
 let oper = "";
 let res = 0;
-
-// display the number that is typed
-// display the operator
-// display the new number
-// display the result 
-
-
+let display = "";
 
 // delet functions 
 // + - function
 // percentage function
 // . function
 // press longer calculations than 2 numbers storgage and stuff
+// and what if we add 
 // division 0 error message
 // add computer kyes to use it also 
+// max length of number 
+
 
 
 
@@ -29,34 +27,57 @@ operators.forEach(operator => operator.addEventListener("click", (e) => {
     oper = e.target.id;
     operator.classList.add('opPressed');
     operator.addEventListener('transitionend', removeTransition);
+    display += displayConversion(oper);
+    calculationDisplay.textContent = display;
 }));
 
 
 numbers.forEach(number => number.addEventListener("click", (e) => {
     num = e.target.id;
+    calculationDisplay.classList.remove('calculationResult');
+    result.textContent = "";
     number.classList.add('opPressed');
     number.addEventListener('transitionend', removeTransition);
     if (oper===""){
-        operant1.push(num);
+        operant1 += num;
+        display += num;
+        calculationDisplay.textContent = display;
     } else if (oper!="") {
-        operant2.push(num);
+        operant2 += num;
+        display += num;
+        calculationDisplay.textContent = display;
     } 
 }));
 
 equal.addEventListener('click', (e) => {
-    operant1 = Number(operant1.join(""));
-    operant2 = Number(operant2.join(""));
+    //operant1 = Number(operant1.join(""));
+    operant1 = Number(operant1);
+    operant2 = Number(operant2);
     res = calculation(oper,operant1, operant2);
-    console.log(res);
-    operant1 = [];
-    operant2 = [];
+    result.textContent = res;
+    calculationDisplay.classList.add('calculationResult')
+    
+    operant1 = "";
+    operant2 = "";
     oper = "";
-
+    display = ""
+    res = "";
+    
+    
 })
 
 
+function displayConversion(oper) {
+    switch(oper) {
+        case 'plus' : return ' + ';
+        case 'minus' : return ' - ';
+        case 'multiply' : return ' × '
+        case 'divide' : return ' ÷ ';
+    };
+
+}
+
 function calculation(operant, a, b){
-    
     switch(operant) {
         case 'plus' : return add(a,b);
         case 'minus' : return subtract(a,b);
