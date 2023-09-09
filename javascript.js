8205e43761ae68bff7a76235bf20e1f6ea7554a3
melinaDaendliker <melina.daendliker@hotmailc.om>
2023-09-09 03:20:57 +0200

    change the functions so also more than a serie of number can be used in calculation

--- javascript.js
+++ javascript.js
@@ -6,24 +6,20 @@ const calculationDisplay = document.querySelector('.calculation');
 const result = document.querySelector('.solution');
 const extras = document.querySelectorAll('.extra');
 
-
 // default values
 let operant1 = "";
 let operant2 = "";
 let oper = "";
 let res = 0;
+let intermediateResult = 0;
 let display = "";
+let numOperators = 0;
+let equalPressed = 0;
 
 // max length of number 
-
-// round display for division
-
-// press longer calculations than 2 numbers storgage and stuff
-// and what if we add operator directly before getting new number storage needs to be different 
-
 // add computer kyes to use it also 
-
 // make code nicer more readable (get function to evaluate which operant is needed)
+
 
 extras.forEach(extra => extra.addEventListener('click', (e) => {
     extra.classList.add('opPressed');
@@ -45,16 +41,28 @@ deleteButtons.forEach(del => del.addEventListener('click', (e) => {
     }
 }))
 
-
 operators.forEach(operator => operator.addEventListener("click", (e) => {
-    oper = e.target.id;
+    numOperators ++;
     operator.classList.add('opPressed');
-    operator.addEventListener('transitionend', removeTransition);
-    display += displayConversion(oper);
+    operator.addEventListener('transitionend', removeTransition); 
+    if (numOperators>1) {
+        res = calculation(oper,Number(operant1), Number(operant2));
+        oper = e.target.id; 
+        operant1 = res;
+        operant2 = '';
+        display = res + ' ' + oper + ' ';
+    } else if (equalPressed==1 && numOperators==1){
+        oper = e.target.id;  
+        operant1 = intermediateResult;
+        display = intermediateResult + ' ' + oper + ' ';
+    } else if (numOperators==1){
+        oper = e.target.id;  
+        display += ' ' + oper + ' ';
+    }
+    intermediateResult = 0;
     calculationDisplay.textContent = display;
 }));
-
-
+  
 numbers.forEach(number => number.addEventListener("click", (e) => {
     num = e.target.id;
     calculationDisplay.classList.remove('calculationResult');
@@ -73,25 +81,25 @@ numbers.forEach(number => number.addEventListener("click", (e) => {
 }));
 
 equal.addEventListener('click', (e) => {
-    console.log(operant1);
-    operant1 = Number(operant1);
-    operant2 = Number(operant2);
-    res = calculation(oper,operant1, operant2);
+    res = calculation(oper,Number(operant1), Number(operant2));
+    intermediateResult = res;
     result.textContent = res;
     calculationDisplay.classList.add('calculationResult')
     
-    // might change later when using longer calculations
     operant1 = "";
     operant2 = "";
     oper = "";
     display = ""
     res = "";
-    
-    
+    numOperators = 0;
+    equalPressed = 1;
 })
 
 function changePlusMinus(){
-    if (operant2 === ""){
+    if (intermediateResult!=0) {
+        intermediateResult = -1* intermediateResult;
+        console.log(intermediateResult);
+     } else if (operant2 === ""){
         if(Number(operant1>0)){
             operant1 = -operant1;
         } 
@@ -99,20 +107,15 @@ function changePlusMinus(){
             operant1 = -1 * operant1;
         }
         display = operant1;
-
-    } else if (operant2 != "") {
+     } else if (operant2 != "") {
         if(Number(operant2>0)){
             operant2 = -operant2;
         } 
         else if(Number(operant2<0)){
             operant2 = -1 * operant2;
         }
-        display = operant1 + displayConversion(oper) + operant2;
-    } //else if (res != "") {
-         // if result not 0
-        // change the sing 
-    //}
-   
+        display = operant1 + ' ' + oper + ' ' + operant2;
+    } 
     calculationDisplay.textContent = display;
 }
 
@@ -128,9 +131,6 @@ function addDot(){
     }
     calculationDisplay.textContent = display
 }
-
-
-
 
 function deleteNumber(){
     if (oper==="" && operant2==="") {
@@ -152,31 +152,20 @@ function deleteAll(){
     oper = "";
     display = ""
     res = "";
+    numOperators = 0;
+    equalPressed = 0;
     result.textContent = "";
     calculationDisplay.textContent = "";
     calculationDisplay.classList.remove('calculationResult');
 }
 
-// remove that not needed when given the right names 
-function displayConversion(oper) {
-    switch(oper) {
-        case 'plus' : return ' + ';
-        case 'minus' : return ' - ';
-        case 'multiply' : return ' × '
-        case 'divide' : return ' ÷ ';
-        case 'power'  : return '^2';
-    };
-
-}
-
-// change this to new names
 function calculation(operant, a, b){
     switch(operant) {
-        case 'plus' : return add(a,b);
-        case 'minus' : return subtract(a,b);
-        case 'multiply' : return multiply(a,b);
-        case 'divide' : return divide(a,b);
-        case  'power' : return power(a)
+        case '+' : return add(a,b);
+        case '-' : return subtract(a,b);
+        case '×' : return multiply(a,b);
+        case '÷' : return divide(a,b);
+        case  '^2' : return power(a)
     };
 }
 
@@ -196,7 +185,7 @@ function divide(a,b){
     if (a==0){
         return 'ERROR'
     }
-    return a / b 
+    return (a / b).toFixed(4); 
 }
 
 function removeTransition(e) {
